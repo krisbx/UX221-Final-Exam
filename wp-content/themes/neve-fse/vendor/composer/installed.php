132fc97c524570fe17ae340e08469ded1cc4fb45
krisbx <kris@banse.ca>
2025-04-23 00:11:06 +0000

    css stuff

---
+++ wp-content/themes/neve-fse/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/neve-fse',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9e91d6565357e5ca0b3428669a795f857d21b6d0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/neve-fse' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9e91d6565357e5ca0b3428669a795f857d21b6d0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.44',
+            'version' => '3.3.44.0',
+            'reference' => 'fed444b52ebf1f689ec2434df177926bf8f238c4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
